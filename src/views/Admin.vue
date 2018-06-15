76dcefe1ee6957acc3dfe07118831554179e348e
Christian Friedow <christian@friedow.com>
2018-06-15 13:01:57 +0200

    Added styling to admin view

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,31 +1,54 @@
 <template>
   <div class="booklist">
-    <input v-on:keyup="searchBooks" type="text" id="searchbar" class="uk-input uk-margin-bottom" placeholder="Search Books">
+    <input v-on:keyup="searchBooks" type="text" id="searchbar" class="uk-input uk-margin-large-bottom" placeholder="Search Books">
     <div class="uk-child-width-1-3@s uk-grid-match uk-grid">
+      <div class="column  uk-margin-bottom">
+        <!-- Post-->
+        <div class="post-module">
+          <!-- Thumbnail-->
+          <div class="thumbnail">
+            <div class="date">
+              <div class="day">27</div>
+              <div class="month">Mar</div>
+            </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg">
+          </div>
+          <!-- Post Content-->
+          <div class="post-content">
+            <div class="category">Photos</div>
+            <h1 class="title">City Lights in New York</h1>
+            <h2 class="sub_title">The city that never sleeps.</h2>
+            <p class="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+            <div class="post-meta"><span class="timestamp"><i class="fa fa-clock-">o</i> 6 mins ago</span><span class="comments"><i class="fa fa-comments"></i><a href="#"> 39 comments</a></span></div>
+          </div>
+        </div>
+      </div>
 
       <div v-for="book in displayedBooks" :key="book.id">
-        <div class="uk-card uk-card-default uk-margin-bottom">
-
-            <div class="uk-card-header">
-                <div class="uk-grid-small uk-flex-middle uk-grid">
-                    <div class="uk-width-expand">
-                        <h3 class="uk-card-title uk-margin-remove-bottom">{{book.title}}</h3>
-                        <p class="uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
-                    </div>
-                </div>
+        <div class="column uk-margin-medium-bottom">
+          <!-- Post-->
+          <div class="post-module">
+            <!-- Thumbnail-->
+            <div class="thumbnail">
+              <div class="date">
+                <div class="day">2017</div>
+                <div class="month">Mar</div>
+              </div><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg">
             </div>
-
-            <div class="uk-card-body">
-                <p>Description</p>
-            </div>
-
-            <div class="uk-card-footer">
-                <a v-on:click="handleBookAction(book, action)" v-for="action in book.actions" :key="action.id" class="uk-button uk-button-text uk-margin-right">
+            <!-- Post Content-->
+            <div class="post-content">
+              <div class="category">Book</div>
+              <h1 class="title">{{book.title}}</h1>
+              <h2 class="sub_title">The city that never sleeps.</h2>
+              <p class="description">New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+              <div class="post-meta">
+                <a class="actions" v-on:click="handleBookAction(book, action)" v-for="action in book.actions" :key="action.id">
                   {{ action.label }}
                 </a>
+              </div>
             </div>
-
+          </div>
         </div>
+
       </div>
 
     </div>
@@ -110,7 +133,7 @@ export default class Booklist extends Vue {
   private addActions(books: Book[]): Book[] {
     books.map(async (book: Book): Promise<Book> => {
       book.actions = await ChimeraApi.getEnabledActivities(book.instanceId);
-      return book
+      return book;
     });
     console.log(books);
     this.books = books;
@@ -133,3 +156,238 @@ export default class Booklist extends Vue {
   // endregion
 }
 </script>
+
+
+<style lang="less">
+
+// Variables
+@white: #FFFFFF;
+@black: #000000;
+@dark_gray: lighten(@black, 20%);
+@gray: lighten(@black, 40%);
+@light_gray: lighten(@black, 60%);
+
+
+// Mixins
+.border-radius( @radius: 3px; ) {
+  -webkit-border-radius: @radius;
+  -moz-border-radius: @radius;
+  border-radius: @radius;
+}
+
+.box-shadow( @h-shadow: 0px; @v-shadow: 1px; @blur: 2px; @spread: 0px; @color: fade(@black, 15%); ) {
+  -webkit-box-shadow: @arguments;
+  -moz-box-shadow: @arguments;
+  box-shadow: @arguments;
+}
+
+.transitions( @property: all; @duration: 0.3s; @timing: linear; @delay: 0s; ) {
+  -webkit-transition: @arguments;
+  -moz-transition: @arguments;
+  -ms-transition: @arguments;
+  -o-transition: @arguments;
+  transition: @arguments;
+}
+
+.box-size( @property: border-box; ) {
+  -webkti-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
+
+.font-smoothing() {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+// Code by Andy Tran
+// Design by JustinKwak
+@accent: #e74c3c;
+
+@container_width: 800px;
+
+body {
+  font-family: 'proxima-nova-soft', sans-serif;
+  font-size: 14px;
+  .font-smoothing;
+}
+
+.post-module {
+  position: relative;
+  z-index: 1;
+  display: block;
+  background: @white;
+  min-width: 270px;
+  height: 470px;
+  .box-shadow;
+  .transitions;
+  &:hover {
+    .box-shadow(@blur: 35px; @color: fade(@black, 30%););
+    .thumbnail {
+      img {
+        -webkit-transform: scale(1.1);
+        -moz-transform: scale(1.1);
+        transform: scale(1.1);
+        opacity: .6;
+      }
+    }
+    .post-content .description {
+      opacity: 1;
+      height: 100px;
+      margin: 20px 0px;
+      visibility: visible;
+    }
+  }
+  .thumbnail {
+    background: @black;
+    height: 400px;
+    overflow: hidden;
+    .date {
+      position: absolute;
+      top: 20px;
+      right: 20px;
+      z-index: 1;
+      background: @accent;
+      width: 55px;
+      height: 55px;
+      padding: 4.5px 0;
+      .border-radius(100%);
+      color: @white;
+      font-weight: 700;
+      text-align: center;
+      .box-size;
+      .day {
+        font-size: 18px;
+      }
+      .month {
+        font-size: 12px;
+        text-transform: uppercase;
+      }
+    }
+    img {
+      display: block;
+      width: 120%;
+      .transitions;
+    }
+  }
+  .post-content {
+    position: absolute;
+    bottom: 0;
+    background: @white;
+    width: 100%;
+    padding: 30px;
+    .box-size;
+    .transitions(@timing: cubic-bezier(.37,.75,.61,1.05));
+    .category {
+      position: absolute;
+      top: -34px;
+      left: 0;
+      background: @accent;
+      padding: 10px 15px;
+      color: @white;
+      font-size: 14px;
+      font-weight: 600;
+      text-transform: uppercase;
+    }
+    .title {
+      margin: 0;
+      padding: 0 0 10px;
+      color: @dark_gray;
+      font-size: 26px;
+      font-weight: 700;
+    }
+    .sub_title {
+      margin: 0;
+      padding: 0 0 20px;
+      color: @accent;
+      font-size: 20px;
+      font-weight: 400;
+    }
+    .description {
+      overflow: hidden;
+      opacity: 0;
+      height: 0;
+      margin: 0;
+      transition: opacity 300ms, height 300ms, margin 300ms;
+      color: @gray;
+      font-size: 14px;
+      line-height: 1.8em;
+    }
+    .post-meta {
+      margin: 30px 0 0;
+      color: @light_gray;
+      .timestamp {
+        margin: 0 16px 0 0;
+      }
+      .actions {
+        text-transform: capitalize;
+      }
+      a {
+        color: @light_gray;
+        text-decoration: none;
+      }
+    }
+  }
+}
+
+.hover {
+  &:extend(.post-module:hover all);
+  .post-content {
+    .description {
+      display: block !important;
+      height: auto !important;
+      opacity: 1 !important;
+    }
+  }
+}
+
+
+
+.container {
+  max-width: @container_width;
+  min-width: 640px;
+  margin: 0 auto;
+  &:before,
+  &:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+  .column {
+    width: 50%;
+    padding: 0 25px;
+    .box-size;
+    float: left;
+    .demo-title {
+      margin: 0 0 15px;
+      color: @gray;
+      font-size: 18px;
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+  }
+  .info {
+    width: 300px;
+    margin: 50px auto;
+    text-align: center;
+    h1 {
+      margin: 0 0 15px;
+      padding: 0;
+      font-size: 24px;
+      font-weight: bold;
+      color: @dark_gray;
+    }
+    span {
+      color: @gray;
+      font-size: 12px;
+      a {
+        color: @black;
+        text-decoration: none;
+      }
+      .fa {
+        color: @accent;
+      }
+    }
+  }
+}
+</style>
