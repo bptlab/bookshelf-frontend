817445dce42f888c8d83145df77dbbfe256324cd
Christian Friedow <christian@friedow.com>
2018-06-18 11:45:04 +0200

    Styled Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,13 @@
 <template>
-    <header class="uk-margin">
-        <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky">
-            <nav class="uk-navbar-container uk-navbar">
+    <header class="uk-margin-large-bottom">
+        <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky; bottom: #transparent-sticky-navbar">
+            <nav class="uk-navbar-container uk-light" uk-navbar="dropbar: true;" style="position: relative; z-index: 980;">
                 <div class="uk-navbar-left">
+                    <ul class="uk-navbar-nav">
+                        <h1 class="logo">BPM Bookshelf</h1>
+                    </ul>
+                </div>
+                <div class="uk-navbar-right">
                     <ul class="uk-navbar-nav">
                         <li>
                             <router-link to="/">Shelf</router-link>
@@ -13,10 +18,6 @@
                         <li>
                             <router-link v-if="loggedIn" to="/admin">Admin</router-link>
                         </li>
-                    </ul>
-                </div>
-                <div class="uk-navbar-right">
-                    <ul class="uk-navbar-nav">
                         <li>
                             <router-link v-if="loggedIn" to="/logout">Log out</router-link>
                             <router-link v-if="!loggedIn" to="/login">Log in</router-link>
@@ -32,6 +33,7 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
 import Authentification from '@/apis/Authentification';
+import UIkit from 'uikit';
 
 @Component
 export default class Header extends Vue {
@@ -54,7 +56,30 @@ export default class Header extends Vue {
     Authentification.onChange = (loggedIn) => {
         this.loggedIn = loggedIn;
     };
+    UIkit.navbar('.uk-navbar-container');
   }
   // endregion
 }
 </script>
+
+<style lang="less">
+
+.uk-navbar-container {
+    background: transparent !important;
+}
+
+.uk-sticky {
+    background: #E34E47;
+}
+
+h1.logo {
+    margin-left: 15px;
+    margin-bottom: 0;
+    font-size: 25px;
+}
+
+h1.logo, .uk-navbar-nav a {
+    color: #EFEFEF !important;
+}
+
+</style>
