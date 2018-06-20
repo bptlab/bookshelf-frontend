aad13c7b1b6f403962f6dda8a9dde580c7adafd7
Christian Friedow <christian@friedow.com>
2018-06-20 10:52:30 +0200

    Extracted initializeBook and filterDataobjectsByState

--- src/Utils.ts
+++ src/Utils.ts
@@ -1,3 +1,8 @@
+import { filter } from 'p-iteration';
+import Dataobject from '@/apis/Chimera/Dataobject';
+import DataobjectAttribute from '@/interfaces/chimera/DataobjectAttribute';
+import Book from '@/interfaces/Book';
+
 export default class Utils {
 
     public static mergeArrays(array: any[][]): any[] {
@@ -9,6 +14,41 @@ export default class Utils {
     public static fetchJson(url: string): Promise<any> {
         return fetch(url).then((response: Response): Promise<Response> => {
             return response.json();
+        });
+    }
+
+    public static async initializeBook(dataobject: Dataobject): Promise<Book> {
+        const book: Book = {
+            title: '',
+            subtitle: '',
+            authors: '',
+            publishedDate: new Date(),
+            description: '',
+            pageCount: 0,
+            language: '',
+            printType: '',
+            category: '',
+            averageRating: 0,
+            imageUrl: '',
+            infoUrl: '',
+        };
+
+        const attributes = await dataobject.attributes;
+        attributes.forEach((attribute: DataobjectAttribute) => {
+                if (attribute.name === 'publishedDate') {
+                    book[attribute.name] = new Date(attribute.value);
+                } else {
+                    book[attribute.name] = attribute.value;
+                }
+            },
+        );
+
+        return book;
+    }
+
+    public static async filterDataobjectsByState(dataobjects: Dataobject[], state: string): Promise<Dataobject[]> {
+        return await filter(dataobjects, async (dataobject: Dataobject): Promise<boolean> => {
+            return await dataobject.state === state;
         });
     }
 }
